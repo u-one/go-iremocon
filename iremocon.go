f192827647d9a3182c36c96bd4eb532b87c08077
Toshiaki Koshiba <bash0C7@users.noreply.github.com>
2016-01-05 00:39:33 +0900

    Implement au, is, ic, cc command

--- iremocon.go
+++ iremocon.go
@@ -3,7 +3,6 @@ package iremocon
 import "fmt"
 import "net"
 import "bufio"
-import "strings"
 
 type Iremocon struct {
 	Conn net.Conn
@@ -16,13 +15,36 @@ func NewIremocon(conn net.Conn) *Iremocon {
 	return i
 }
 
+func (iremocon *Iremocon) Au() string {
+	res := iremocon.send("au")
+	return res
+}
+
+func (iremocon *Iremocon) Is(ch string) string {
+	res := iremocon.send("is", ch)
+	return res
+}
+
+func (iremocon *Iremocon) Ic(ch string) string {
+	res := iremocon.send("ic", ch)
+	return res
+}
+
+func (iremocon *Iremocon) Cc() string {
+	res := iremocon.send("cc")
+	return res
+}
+
 func (iremocon *Iremocon) Vr() string {
 	res := iremocon.send("vr")
 	return res
 }
 
 func (iremocon *Iremocon) send(command string, params ...string) string {
-	param := strings.Join(params, ";")
+	param := ""
+	for _, p := range params {
+		param += ";" + p
+	}
 	fmt.Fprintf(iremocon.Conn, "*%v%v\r\n", command, param)
 	res, _ := bufio.NewReader(iremocon.Conn).ReadString('\n')
 	return res
--- iremocon_test.go
+++ iremocon_test.go
@@ -1,20 +1,51 @@
 package iremocon
 
-import "fmt"
 import "net"
 import "testing"
 
+import "io"
+
+func runEchoServer(listener net.Listener) {
+	for {
+		conn, _ := listener.Accept()
+		go func(c net.Conn) {
+			io.Copy(c, c)
+			c.Close()
+		}(conn)
+	}
+}
+
 func TestIremocon(t *testing.T) {
-	conn, err := net.Dial("tcp", "10.0.1.200:51013")
-	if err != nil {
-		fmt.Printf("error")
+	laddr := ":50000"
+	ln, _ := net.Listen("tcp", laddr)
+	defer ln.Close()
+	go runEchoServer(ln)
+
+	conn, _ := net.Dial("tcp", laddr)
+	iremocon := NewIremocon(conn)
+	echo := ""
+	echo = iremocon.Au()
+	if echo != "*au\r\n" {
+		t.Errorf("au error: %v", echo)
 	}
 
-	iremocon := NewIremocon(conn)
-	result := iremocon.Vr()
-	if result != "1.0.0\r\n" {
-		t.Fatalf("vr error: %v", result)
-	} else {
-		t.Log(result)
+	echo = iremocon.Is("1")
+	if echo != "*is;1\r\n" {
+		t.Errorf("is error: %v", echo)
+	}
+
+	echo = iremocon.Ic("1")
+	if echo != "*ic;1\r\n" {
+		t.Errorf("ic error: %v", echo)
+	}
+
+	echo = iremocon.Cc()
+	if echo != "*cc\r\n" {
+		t.Errorf("cc error: %v", echo)
+	}
+
+	echo = iremocon.Vr()
+	if echo != "*vr\r\n" {
+		t.Errorf("vr error: %v", echo)
 	}
 }
