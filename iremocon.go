a686a2719072971e8726aa50189fe94dc9c11fca
Toshiaki Koshiba <bash0C7@users.noreply.github.com>
2016-01-08 00:40:06 +0900

    Implement error handling

--- iremocon.go
+++ iremocon.go
@@ -4,6 +4,8 @@ import "fmt"
 import "net"
 import "bufio"
 import "strconv"
+import "strings"
+import "errors"
 
 type Iremocon struct {
 	Conn net.Conn
@@ -16,62 +18,68 @@ func NewIremocon(conn net.Conn) *Iremocon {
 	return i
 }
 
-func (iremocon *Iremocon) Au() string {
-	res := iremocon.send("au")
-	return res
+func (iremocon *Iremocon) Au() (string, error) {
+	res, err := iremocon.send("au")
+	return res, err
 }
 
-func (iremocon *Iremocon) Is(ch int) string {
-	res := iremocon.send("is", strconv.Itoa(ch))
-	return res
+func (iremocon *Iremocon) Is(ch int) (string, error) {
+	res, err := iremocon.send("is", strconv.Itoa(ch))
+	return res, err
 }
 
-func (iremocon *Iremocon) Ic(ch int) string {
-	res := iremocon.send("ic", strconv.Itoa(ch))
-	return res
+func (iremocon *Iremocon) Ic(ch int) (string, error) {
+	res, err := iremocon.send("ic", strconv.Itoa(ch))
+	return res, err
 }
 
-func (iremocon *Iremocon) Cc() string {
-	res := iremocon.send("cc")
-	return res
+func (iremocon *Iremocon) Cc() (string, error) {
+	res, err := iremocon.send("cc")
+	return res, err
 }
 
-func (iremocon *Iremocon) Tm(ch int, time int, interval int) string {
-	res := iremocon.send("tm", strconv.Itoa(ch), strconv.Itoa(time), strconv.Itoa(interval))
-	return res
+func (iremocon *Iremocon) Tm(ch int, time int, interval int) (string, error) {
+	res, err := iremocon.send("tm", strconv.Itoa(ch), strconv.Itoa(time), strconv.Itoa(interval))
+	return res, err
 }
 
-func (iremocon *Iremocon) Tl() string {
-	res := iremocon.send("tl")
-	return res
+func (iremocon *Iremocon) Tl() (string, error) {
+	res, err := iremocon.send("tl")
+	return res, err
 }
 
-func (iremocon *Iremocon) Td(timerId int) string {
-	res := iremocon.send("td", strconv.Itoa(timerId))
-	return res
+func (iremocon *Iremocon) Td(timerId int) (string, error) {
+	res, err := iremocon.send("td", strconv.Itoa(timerId))
+	return res, err
 }
 
-func (iremocon *Iremocon) Ts(time int) string {
-	res := iremocon.send("ts", strconv.Itoa(time))
-	return res
+func (iremocon *Iremocon) Ts(time int) (string, error) {
+	res, err := iremocon.send("ts", strconv.Itoa(time))
+	return res, err
 }
 
-func (iremocon *Iremocon) Tg() string {
-	res := iremocon.send("tg")
-	return res
+func (iremocon *Iremocon) Tg() (string, error) {
+	res, err := iremocon.send("tg")
+	return res, err
 }
 
-func (iremocon *Iremocon) Vr() string {
-	res := iremocon.send("vr")
-	return res
+func (iremocon *Iremocon) Vr() (string, error) {
+	res, err := iremocon.send("vr")
+	return res, err
 }
 
-func (iremocon *Iremocon) send(command string, params ...string) string {
+func (iremocon *Iremocon) send(command string, params ...string) (string, error) {
 	param := ""
 	for _, p := range params {
 		param += ";" + p
 	}
 	fmt.Fprintf(iremocon.Conn, "*%v%v\r\n", command, param)
-	res, _ := bufio.NewReader(iremocon.Conn).ReadString('\n')
-	return res
+	res, err := bufio.NewReader(iremocon.Conn).ReadString('\n')
+	if err != nil {
+		return res, err
+	}
+	if strings.HasPrefix(res, ("*" + command + ";err;")) {
+		return res, errors.New(res)
+	}
+	return res, err
 }
--- iremocon_test.go
+++ iremocon_test.go
@@ -2,12 +2,17 @@ package iremocon
 
 import "net"
 import "testing"
-
+import "bufio"
 import "io"
 
-func runEchoServer(listener net.Listener) {
+const laddr = ":50000"
+
+func runEchoServer(listener net.Listener, t *testing.T) {
 	for {
-		conn, _ := listener.Accept()
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Fatal(err)
+		}
 		go func(c net.Conn) {
 			io.Copy(c, c)
 			c.Close()
@@ -15,62 +20,177 @@ func runEchoServer(listener net.Listener) {
 	}
 }
 
+func runErrorServer(listener net.Listener, t *testing.T) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Fatal(err)
+		}
+		go func(c net.Conn) {
+			r := bufio.NewReader(c)
+			for {
+				req, rerr := r.ReadString('\n')
+				if rerr != nil {
+					break
+				}
+				res := req[0:3] + ";err;000\r\n"
+				c.Write([]byte(res))
+			}
+			c.Close()
+		}(conn)
+	}
+}
+
 func TestIremocon(t *testing.T) {
-	laddr := ":50000"
 	ln, _ := net.Listen("tcp", laddr)
 	defer ln.Close()
-	go runEchoServer(ln)
+	go runEchoServer(ln, t)
 
 	conn, _ := net.Dial("tcp", laddr)
+	defer conn.Close()
 	iremocon := NewIremocon(conn)
-	echo := ""
-	echo = iremocon.Au()
+
+	var echo string
+	var err error
+	echo, err = iremocon.Au()
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*au\r\n" {
 		t.Errorf("au error: %v", echo)
 	}
 
-	echo = iremocon.Is(1)
+	echo, err = iremocon.Is(1)
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*is;1\r\n" {
 		t.Errorf("is error: %v", echo)
 	}
 
-	echo = iremocon.Ic(1)
+	echo, err = iremocon.Ic(1)
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*ic;1\r\n" {
 		t.Errorf("ic error: %v", echo)
 	}
 
-	echo = iremocon.Cc()
+	echo, err = iremocon.Cc()
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*cc\r\n" {
 		t.Errorf("cc error: %v", echo)
 	}
 
-	echo = iremocon.Tm(1, 1577804400, 10)
+	echo, err = iremocon.Tm(1, 1577804400, 10)
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*tm;1;1577804400;10\r\n" {
 		t.Errorf("tm error: %v", echo)
 	}
 
-	echo = iremocon.Tl()
+	echo, err = iremocon.Tl()
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*tl\r\n" {
 		t.Errorf("tl error: %v", echo)
 	}
 
-	echo = iremocon.Td(1)
+	echo, err = iremocon.Td(1)
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*td;1\r\n" {
 		t.Errorf("td error: %v", echo)
 	}
 
-	echo = iremocon.Ts(1577804400)
+	echo, err = iremocon.Ts(1577804400)
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*ts;1577804400\r\n" {
 		t.Errorf("ts error: %v", echo)
 	}
 
-	echo = iremocon.Tg()
+	echo, err = iremocon.Tg()
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*tg\r\n" {
 		t.Errorf("tg error: %v", echo)
 	}
 
-	echo = iremocon.Vr()
+	echo, err = iremocon.Vr()
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
 	if echo != "*vr\r\n" {
 		t.Errorf("vr error: %v", echo)
+	}
+}
+
+func TestIremoconError(t *testing.T) {
+	ln, _ := net.Listen("tcp", laddr)
+	defer ln.Close()
+	go runErrorServer(ln, t)
+
+	conn, _ := net.Dial("tcp", laddr)
+	defer conn.Close()
+	iremocon := NewIremocon(conn)
+
+	var res string
+	var err error
+	res, err = iremocon.Au()
+	if err == nil {
+		t.Errorf("no error: %v", res)
+	}
+
+	res, err = iremocon.Is(1)
+	if err == nil {
+		t.Errorf("no error: %v", res)
+	}
+
+	res, err = iremocon.Ic(1)
+	if err == nil {
+		t.Errorf("no error: %v", res)
+	}
+
+	res, err = iremocon.Cc()
+	if err == nil {
+		t.Errorf("no error: %v", res)
+	}
+
+	res, err = iremocon.Tm(1, 1577804400, 10)
+	if err == nil {
+		t.Errorf("no error: %v", res)
+	}
+
+	res, err = iremocon.Tl()
+	if err == nil {
+		t.Errorf("no error: %v", res)
+	}
+
+	res, err = iremocon.Td(1)
+	if err == nil {
+		t.Errorf("no error: %v", res)
+	}
+
+	res, err = iremocon.Ts(1577804400)
+	if err == nil {
+		t.Errorf("no error: %v", res)
+	}
+
+	res, err = iremocon.Tg()
+	if err == nil {
+		t.Errorf("no error: %v", res)
+	}
+
+	res, err = iremocon.Vr()
+	if err == nil {
+		t.Errorf("no error: %v", res)
 	}
 }
